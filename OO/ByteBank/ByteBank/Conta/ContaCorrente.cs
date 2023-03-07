246afbbfc7b21867d4b613b2d4355af555ec4901
felipeSantosf <lipesantos.gtt@gmail.com>
2023-03-06 22:50:43 -0300

    Add conteudo de POO

---
+++ OO/ByteBank/ByteBank/Conta/ContaCorrente.cs
@@ -0,0 +1,57 @@
+﻿using ByteBank.Titular;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.Conta
+{
+    public class ContaCorrente
+    {
+        public int numero_agencia;
+        public string conta;
+        public double saldo = 100;
+
+        public Cliente titular;
+
+        public void Depositar(double valor)
+        {
+            saldo += valor;
+        }
+
+        public bool Sacar(double valor)
+        {
+            if (saldo >= valor)
+            {
+                saldo -= valor;
+                return true;
+            }
+
+            return false;
+        }
+
+
+        public bool Transferir(double valor, ContaCorrente contaDestino)
+        {
+            if (saldo >= valor)
+            {
+                contaDestino.saldo += valor;
+                saldo -= valor;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void exibirInformacoesConta()
+        {
+            Console.WriteLine("Número da agência: " + numero_agencia);
+            Console.WriteLine("Número da conta: " + conta);
+            Console.WriteLine("Titular: " + titular);
+            Console.WriteLine("Saldo: " + saldo);
+        }
+    }
+}
---
+++ OO/ByteBank/ByteBank/Program.cs
@@ -0,0 +1,95 @@
+﻿using ByteBank.Conta;
+using ByteBank.Titular;
+
+//ContaCorrente contaDoFelipe = new ContaCorrente();
+//contaDoFelipe.titular = "Felipe Santos";
+//contaDoFelipe.numero_agencia = 15;
+//contaDoFelipe.conta = "4598-X";
+//contaDoFelipe.saldo = 23.77;
+
+//ContaCorrente contaDoFelipe2 = new ContaCorrente();
+//contaDoFelipe.titular = "Felipe Santos";
+//contaDoFelipe.numero_agencia = 15;
+//contaDoFelipe.conta = "4598-X";
+//contaDoFelipe.saldo = 23.77;
+
+// TIPAGEM POR REFERÊNCIA
+//contaDoFelipe = contaDoFelipe2;
+
+//Console.WriteLine(contaDoFelipe == contaDoFelipe2);
+
+
+//ContaCorrente contaDaMaria = new ContaCorrente();
+//contaDaMaria.titular = "Maria Silveisa";
+//contaDaMaria.numero_agencia = 58;
+//contaDaMaria.conta = "6699-X"; 
+//contaDaMaria.saldo = 80.66;
+
+//Console.WriteLine("Saldo da conta do Felipe =  " + contaDoFelipe.saldo);
+
+//contaDoFelipe.Depositar(155.63);
+
+//Console.WriteLine("Saldo da conta do Felipe após depósito =  " + contaDoFelipe.saldo);
+//Console.WriteLine("\n\n\n");
+
+//contaDoFelipe.exibirInformacoesConta();
+
+//if (contaDoFelipe.Sacar(50))
+//{
+//    Console.WriteLine("Saldo da conta do Felipe após saque =  " + contaDoFelipe.saldo);
+//}
+//else
+//{
+//    Console.WriteLine("Saldo insuficiente para saque.");
+//}
+
+
+//if (contaDoFelipe.Transferir(60.98, contaDaMaria))
+//{
+//    Console.WriteLine("Tranferência realizada com sucesso, saldo após transferência = " + contaDoFelipe.saldo);
+//}
+//else
+//{
+//    Console.WriteLine("Saldo insuficiente, não foi possível realizar a solicitação");
+//}
+
+// TIPAGEM POR VALOR
+//double valor = 300;
+//double valor2 = valor;
+
+//Console.WriteLine(valor == valor2);
+//Console.WriteLine(valor);
+//Console.WriteLine(valor2);
+
+
+
+
+Cliente cliente = new Cliente();
+cliente.nome = "Felipe José";
+cliente.CPF = "1234568903";
+cliente.profissao = "Frentista";
+
+ContaCorrente conta = new ContaCorrente();
+conta.titular = cliente;
+conta.conta = "1342-X";
+conta.numero_agencia = 1;
+conta.saldo = 100;
+
+Console.WriteLine("Titular = " + conta.titular.nome);
+Console.WriteLine("CPF = " + conta.titular.CPF);
+Console.WriteLine("Profissao = " + conta.titular.profissao);
+Console.WriteLine("N° Conta = " + conta.conta);
+Console.WriteLine("Saldo = " + conta.saldo);
+Console.WriteLine("N° agência = " + conta.numero_agencia);
+
+
+ContaCorrente conta2 = new ContaCorrente();
+conta2.titular = new Cliente();
+conta2.titular.nome = "Maria Joaquina";
+conta2.titular.profissao = "Analista";
+conta2.titular.CPF = "8473269821";
+conta2.conta = "9999-X";
+conta2.numero_agencia = 89;
+conta2.saldo = 900;
+
+Console.WriteLine(conta2.titular.nome);
